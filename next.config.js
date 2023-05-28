43c64b41a3f6f92ca3414649d535c85b3f4eb419
unknown <tariqatlas7@gmail.com>
2023-05-27 18:35:34 -0700

    fixing import setting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-import { PHASE_PRODUCTION_BUILD } from 'next/constants'
-import { spawn } from 'child_process'
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
 module.exports = phase => {
   if (phase === PHASE_PRODUCTION_BUILD) {
+    const { spawn } = require('child_process')
     spawn('cp', ['-a', 'public', 'out/'])
   }
   return {}
