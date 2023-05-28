52a352ac41695e4b5ba401e9aac9ca8c1f2bfed5
unknown <tariqatlas7@gmail.com>
2023-05-27 18:30:18 -0700

    updating next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+import { PHASE_PRODUCTION_BUILD } from 'next/constants'
+import { spawn } from 'child_process'
+
+module.exports = phase => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    spawn('cp', ['-a', 'public', 'out/'])
+  }
+  return {}
+}
